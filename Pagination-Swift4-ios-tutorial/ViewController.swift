6ccadf55b3f955ce437f1ff6ad5c632166e7dc84
leoiphonedev <aggarwalaman1985@gmail.com>
2018-05-04 21:02:12 +0530

    Initial Commit

---
+++ Pagination-Swift4-ios-tutorial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pagination-Swift4-ios-tutorial
+//
+//  Created by Aman Aggarwal on 04/05/18.
+//  Copyright © 2018 Aman Aggarwal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
